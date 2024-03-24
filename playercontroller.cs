fd0dcc702444bba3b9a229954deb715c4954c6b1
Martin Pezous <66411147+LoPezous@users.noreply.github.com>
2024-03-24 15:22:23 +0100

    Edited variable names
    Edited variable names to follow C# naming conventions
    Le cylinddre bouge je répète le cylindre bouge !

--- playercontroller.cs
+++ playercontroller.cs
@@ -3,27 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class playercontroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {   
     public float speed;
     private Vector2 move;
+
+    // This method is called by the Input System whenever there is a movement input
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer(); // Call the movement function from Update
     }
 
-    public void movePlayer()
+    // Function to handle player movement
+    private void MovePlayer()
     {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
